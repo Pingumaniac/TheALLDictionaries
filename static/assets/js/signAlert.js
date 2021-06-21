e913863d68855be291a89f27687b522e3fad251c
Pingumaniac <63883314+Pingumaniac@users.noreply.github.com>
2021-06-21 23:51:25 +0900

    Delete signAlert.js

--- static/assets/js/signAlert.js
+++
@@ -1,9 +0,0 @@
-function SignOut() {
-    alert("You have been successfully logged out.");
-}
-function SignIn() {
-    alert("Please sign in.");
-}
-function SignUp() {
-    alert("Please sign up.");
-}
